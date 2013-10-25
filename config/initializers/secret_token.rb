a4800830cd942a5465e03b61a06bb321434d6f1c
Hugo Henley <hugohenley@gmail.com>
2013-10-25 16:39:46 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdi::Application.config.secret_token = 'bcae21e96ded3c0479f849417ba2826aa0b6901c3ad457e75908ed273f3b135c3cfc1684efe09726443704e81ba0f3cec4b3331af5e962dc33c84cf60c609fb8'
