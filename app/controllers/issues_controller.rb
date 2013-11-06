fa003ffc06a599d5ec02f2980e4361a22236759f
Hugo Henley <hugohenley@gmail.com>
2013-11-06 12:41:36 -0200

    Adicionando IDs de produção e colocando aviso de submissão com sucesso/erro na tela

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -5,21 +5,27 @@ class IssuesController < ApplicationController
   end
 
   def create
-    issue_params = IssueService.new(params["issue"], "proposta_projeto", params["metodologia"] , params["orcamento"])
+    issue_params = IssueService.new(params["issue"], "proposta_projeto", params["metodologia"], params["orcamento"])
     issue_params.generate_needed_params
     issue = Issue.new(issue_params.params)
     issue.add_custom_field_values(params, usuario_corrente)
 
-    if issue.save
-      metodologia = Metodologia.new(params["metodologia"], issue.subject, issue.id)
-      metodologia.save
+    begin
+      if issue.save
+        metodologia = Metodologia.new(params["metodologia"], issue.subject, issue.id)
+        metodologia.save
 
-      orcamento = Orcamento.new(params["orcamento"], issue.subject, issue.id)
-      orcamento.save
+        orcamento = Orcamento.new(params["orcamento"], issue.subject, issue.id)
+        orcamento.save
 
-      redirect_to root_path, :notice => "Projeto submetido com sucesso!"
-    else
-      render :new, :alert => "Houve algum problema com o envio de seus dados."
+        redirect_to root_path, :notice => "Projeto submetido com sucesso!"
+      else
+        flash[:error] = "Erro ao submeter os dados."
+        render :new
+      end
+    rescue
+      flash[:error] = "Houve um problema de comunicação com o servidor. Tente novamente mais tarde."
+      render :new
     end
   end
 
